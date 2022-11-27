7f64e8d99631bc6331b13cf3f7405ebf511ec188
João Pedro T <joaopedrot1912@gmail.com>
2022-11-27 17:50:40 -0300

    update 27/11

--- ex09/script.js
+++ ex09/script.js
@@ -22,92 +22,124 @@ const igual = document.getElementById("tecigual")
 let valortot = 0
 let valor = ""
 let sinal = ""
+let numero = 0
+let detec_tela = 0
+
+let calc = {
+    valor_amz: 0,
+    sinal_amz: undefined
+}
 
 um.addEventListener("click", function(){
+    if (detec_tela == 1) {
+        valor = ""
+        detec_tela = 0
+        telaExibidora.innerHTML = ``
+    }
     valor += "1"
+    numero = 1
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 dois.addEventListener("click", function(){
+    if (detec_tela == 1) {
+        valor = ""
+        detec_tela = 0
+        telaExibidora.innerHTML = ``
+    }
     valor += "2"
+    numero = 2
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 tres.addEventListener("click", function(){
+    numero = 3
     valor += "3"
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 quatro.addEventListener("click", function(){
     valor += "4"
+    numero = 4
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 cinco.addEventListener("click", function(){
     valor += "5"
+    numero = 5
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 seis.addEventListener("click", function(){
     valor += "6"
+    numero = 6
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 sete.addEventListener("click", function(){
     valor += "7"
+    numero = 7
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 oito.addEventListener("click", function(){
     valor += "8"
+    numero = 8
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 nove.addEventListener("click", function(){
     valor += "9"
+    numero = 9
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 zero.addEventListener("click", function(){
     valor += "0"
+    numero = 0
     telaExibidora.innerHTML = `${valor}`
-    calculador()
 })
 
 
 
 dividir.addEventListener("click", function(){
-    sinal = "divisao"
-    valorSecundario = valor
-    valor = "0"
-    calculador()
+    if (calc.sinal_amz != undefined) {
+        calculador()
+    } else {
+        calc.sinal_amz = "divisao"
+        calc.valor_amz = numero
+        valor = ""
+    }
 })
 
 soma.addEventListener("click", function(){
-    sinal = "soma"
-    valor = "0"
-    calculador()
+    if (calc.sinal_amz != undefined) {
+        calculador()
+    } else {
+        calc.sinal_amz = "soma"
+        calc.valor_amz = numero
+        valor = ""
+    }
 })
 
 subtracao.addEventListener("click", function(){
-    sinal = "subtracao"
-    valor = "0"
-    calculador()
+    if (calc.sinal_amz != undefined) {
+        calculador()
+    } else {
+        calc.sinal_amz = "subtracao"
+        calc.valor_amz = numero
+        valor = ""
+    }
 })
 
 porcento.addEventListener("click", function(){
-    sinal = "porcentagem"
-    valor = "0"
-    calculador()
+    if (calc.sinal_amz != undefined) {
+        calculador()
+    } else {
+        calc.sinal_amz = "porcentagem"
+        calc.valor_amz = numero
+        valor = ""
+    }
 })
 
 igual.addEventListener("click", function(){
@@ -117,10 +149,24 @@ igual.addEventListener("click", function(){
 
 
 function calculador() {
-    if (sinal == "divisao") {
-        valor = Number(valorSecundario) / Number(valor)
+    let resultado = 0
+    if (calc.sinal_amz == "divisao") {
+        resultado = (calc.valor_amz / numero)
     }
-
+    if(calc.sinal_amz == "soma") {
+        resultado = (calc.valor_amz + numero)
+    }
+    if(calc.sinal_amz == "subtracao") {
+        resultado = (calc.valor_amz - numero)
+    }
+    if(calc.sinal_amz == "porcentagem") {
+        resultado = ((calc.valor_amz * numero) / 100)
+    }
+    if(calc.sinal_amz == "multiplicacao") {
+        resultado = (calc.valor_amz * numero)
+    }
+    detec_tela = 1
+    telaExibidora.innerHTML = `${resultado}`
 
 }
 
@@ -128,6 +174,9 @@ function limpar(){
 
     telaExibidora.innerHTML = ""
     valor = ""
+    numero = 0
     sinal = ""
+    calc.valor_amz = 0
+    calc.sinal_amz = undefined
 }
 
